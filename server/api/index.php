f45e2b171ab7a46e7eaaec566d8eaf72290a51e4
Andrew Spode <andrew.spode@itech.media>
2021-08-25 12:05:16 +0100

    - ip update

--- server/api/index.php
+++ server/api/index.php
@@ -2,10 +2,7 @@
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 
-use TeamSix\Helper\Response;
+use TeamSix\Helper\Generator\FilenameGenerator;
 
-var_dump(get_class_methods(TeamSix\Helper\Response::class));
-exit;  
-
-echo Response::generateNewFilename('.png'); 
+echo FilenameGenerator::generateNewFilename('.png'); 
 exit; 
