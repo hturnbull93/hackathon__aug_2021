77921d7c8e3ab4ab904b3da02a6a6565e110b7a2
Harry Turnbull <harry.turnbull@itech.media>
2021-08-25 12:57:40 +0100

    uninstall dropzone

--- client/src/main.js
+++ client/src/main.js
@@ -2,9 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import DropZone from "dropzone-vue";
-import "dropzone-vue/dist/dropzone-vue.common.css";
-
 const app = createApp(App);
 
 app.use(DropZone);
