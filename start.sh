0961dc1eb4d92d30cc2936b0779420bbbea2a1c3
Andrew Spode <andrew.spode@itech.media>
2021-08-25 11:20:19 +0100

    - added GD libraries

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+# This should be run inside a screen session
+
 docker-compose down
 docker-compose up --build -d
 
 while [ : ]
 do
-    git pull
+    git fetch --all
+    git reset --hard
+    sleep 5
 done
