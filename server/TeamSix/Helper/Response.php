a24202c12d65634d1fa2866068954399e07ff9af
Andrew Spode <andrew.spode@itech.media>
2021-08-25 13:43:07 +0100

    - CORS

--- server/TeamSix/Helper/Response.php
+++ server/TeamSix/Helper/Response.php
@@ -6,6 +6,8 @@ class Response
 {
     public static function sendFormattedResponse(array $response): void
     {
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Headers: *");
         header('Content-Type: application/json', '', 200);
         echo json_encode($response); 
         exit; 
