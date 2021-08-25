201978e1ec94101412940fa8f8789e2493cf491b
Andrew Spode <andrew.spode@itech.media>
2021-08-25 14:24:38 +0100

    - removing comments

--- server/api/upload/index.php
+++ server/api/upload/index.php
@@ -5,40 +5,14 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 use TeamSix\Helper\Response; 
 use TeamSix\Helper\Generator\FilenameGenerator; 
 
-/*
-$_FILES -> contain file
-$_REQUEST -> metadata 
-
-           [name] => MyFile.jpg
-            [type] => image/jpeg
-            [tmp_name] => /tmp/php/php6hst32
-            [error] => UPLOAD_ERR_OK
-            [size] => 98174
-
-
-            https://www.php.net/manual/en/function.move-uploaded-file.php
-
-*/
-
-
-//loop through $_FILES 
-// create new upload request for each item in array
-// then use tmp_name and generated filename and pass to move_uploaded_file (referencing data directory where image goes)
-
-//Form elemen
-
 $response = [
     'success' => true
 ]; 
-
 
 $filename = FilenameGenerator::generateNewFilename(); 
 
 // Process it for security
 $image = imagecreatefromstring(base64_decode($_REQUEST['file']));
-
-var_dump($_REQUEST);
-die();
 
 if ($image) {
     //storing image
@@ -50,12 +24,6 @@ if ($image) {
 } else {
     $response['success'] = false;
 }
-
-/* 
-Index code
-*/
-
-
 
 Response::sendFormattedResponse($response); 
 exit; 
