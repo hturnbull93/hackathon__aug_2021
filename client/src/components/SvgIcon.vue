39bbcba1be7d17c26779ab64c7df9085780e8dcf
Oli Hepworth-Bell <Oli.Hepworth-Bell@itech.media>
2021-08-26 13:47:40 +0100

    Added SvgIcon component

---
+++ client/src/components/SvgIcon.vue
@@ -0,0 +1,33 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg">
+    <title v-if="title">{{ title }}</title>
+    <use :href="iconPath" xmlns:xlink="http://www.w3.org/1999/xlink" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "SvgIcon",
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      default: null
+    }
+  },
+  computed: {
+    iconPath() {
+      let icon = require(`@/assets/sprites/${this.name}.svg`);
+
+      if (Object.prototype.hasOwnProperty.call(icon, "default")) {
+        icon = icon.default;
+      }
+
+      return icon.url;
+    }
+  }
+};
+</script>
--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,20 @@
 module.exports = {
-    devServer: {
-      disableHostCheck: true
+  chainWebpack: config => {
+    config.module
+      .rule("svg-sprite")
+      .use("svgo-loader")
+      .loader("svgo-loader");
+  },
+  pluginOptions: {
+    svgSprite: {
+      dir: "src/assets/sprites",
+      test: /\.svg$/,
+      pluginOptions: {
+        plainSprite: true
+      }
     }
+  },
+  devServer: {
+    disableHostCheck: true
   }
+};
